79b7aeff407aacebe8a20c08add566189312ff4f
KleberMera <klebermera2016@gmail.com>
2024-09-29 14:42:16 -0500

    modified css: auth

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Auth } from '../../models/auth.models';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private apiUrlBase = environment.aguapenApi;
+  http = inject(HttpClient);
 
-
+  login(auth: Auth): Observable<Auth> {
+    const url = `${this.apiUrlBase}login`;
+    return this.http.post<Auth>(url, { auth });
+  }
 }
